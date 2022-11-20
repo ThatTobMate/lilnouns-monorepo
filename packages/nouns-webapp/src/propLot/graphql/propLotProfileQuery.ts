8030decef756efc4bda1ebf680617512b898dd0e
Tobias Hale <tobias.hale@deliveroo.co.uk>
2022-11-20 21:09:57 +0000

    proplot profile query schema

---
+++ packages/nouns-webapp/src/propLot/graphql/propLotProfileQuery.ts
@@ -0,0 +1,77 @@
+import { gql } from '@apollo/client';
+
+export const GET_PROPLOT_PROFILE_QUERY = gql`
+  query getPropLotProfile($options: PropLotProfileInputOptions!) {
+    propLotProfile: getPropLotProfile(options: $options) {
+      profile {
+        user {
+          wallet
+          lilnounCount
+          userStats {
+            totalVotes
+            totalUpvotes
+            totalDownvotes
+            totalComments
+            totalIdeas
+            netVotesReceived
+            downvotesReceived
+            upvotesReceived
+          }
+        }
+      }
+      list {
+        ... on Idea {
+          id
+          title
+          tldr
+          creatorId
+          description
+          votecount
+          createdAt
+          ideaStats {
+            comments
+          }
+          closed
+          consensus
+          tags {
+            type
+            label
+          }
+          votes {
+            id
+            voterId
+            ideaId
+            direction
+            voter {
+              wallet
+              lilnounCount
+            }
+          }
+        }
+      }
+      sortFilter {
+        ...filterProperties
+      }
+      tabFilter {
+        ...filterProperties
+      }
+      metadata {
+        requestUUID
+        appliedFilters
+      }
+    }
+  }
+
+  fragment filterProperties on PropLotFilter {
+    id
+    type
+    label
+    options {
+      id
+      label
+      selected
+      value
+      icon
+    }
+  }
+`;
