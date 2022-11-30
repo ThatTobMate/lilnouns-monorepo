2275bec45ee623d276ee11ede0d52e475f6e033e
Tobias Hale <tobias.hale@deliveroo.co.uk>
2022-11-30 14:32:02 +0000

    Cleanup ProfileGovernanceList, move datafetching into hook and pass in as props

--- packages/nouns-webapp/src/propLot/components/ProfileGovernanceList/index.tsx
+++ packages/nouns-webapp/src/propLot/components/ProfileGovernanceList/index.tsx
@@ -1,60 +1,41 @@
 import { Spinner, Alert } from 'react-bootstrap';
 
-import { formatSubgraphProposal, ProposalState } from '../../../wrappers/nounsDao';
-import { formatBigNounSubgraphProposal } from '../../../wrappers/bigNounsDao';
+import { ProposalState } from '../../../wrappers/nounsDao';
 import { bigNounsPropStatus } from '../../../components/Proposals';
-import classes from '../../../pages/Governance/Governance.module.css';
-import { useLazyQuery } from '@apollo/client';
-import { useEffect, useState, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import { FilterType as FilterTyeEnum } from '../../graphql/__generated__/globalTypes';
-import ProfileTabFilters, { GenericFilter } from '../ProfileTabFilters';
+import { useState } from 'react';
+import { TabOption, TabWrapper } from '../ProfileTabFilters';
 import ProposalStatus from '../../../components/ProposalStatus';
-import {
-  LIL_NOUNS_GOVERNANCE_BY_OWNER_SUB,
-  SNAPSHOT_GOVERNANCE_BY_OWNER_SUB,
-  BIG_NOUNS_PROPOSALS_SUB,
-} from '../../../wrappers/subgraph';
-import { useBlockMeta, useBlockNumber } from '@usedapp/core';
-import { useBlockTimestamp } from '../../../hooks/useBlockTimestamp';
+import { TabFilterOptions, TabFilterOptionValues } from '../../hooks/useProfileGovernanceData';
 
-const filter: GenericFilter = {
-  id: 'GOVERNANCE',
-  type: FilterTyeEnum.SINGLE_SELECT,
-  label: 'Tabs',
+import { createBreakpoint } from 'react-use';
+import DropdownFilter from '../DropdownFilter';
+import { FilterType as FilterTypeEnum } from '../../graphql/__generated__/globalTypes';
+
+const useBreakpoint = createBreakpoint({ XL: 1440, L: 940, M: 650, S: 540 });
+
+const sortFilter: any = {
   __typename: 'PropLotFilter',
+  id: 'GovernanceFilter',
+  type: FilterTypeEnum.SINGLE_SELECT,
+  label: 'Filter',
   options: [
     {
-      id: 'YES',
-      label: 'Voted Yes',
-      selected: true,
-      value: 'YES',
-      icon: null,
-      __typename: 'FilterOption',
+      id: `ALL`,
+      selected: false,
+      value: 'ALL',
+      label: 'All',
     },
     {
-      id: 'NO',
-      label: 'Voted No',
+      id: `LIL_NOUNS`,
       selected: false,
-      value: 'NO',
-      icon: null,
-      __typename: 'FilterOption',
+      value: 'LIL_NOUN',
+      label: 'Lil Nouns DAO',
     },
     {
-      id: 'ABSTAINED',
-      label: 'Abstained',
+      id: `NOUNS`,
       selected: false,
-      value: 'ABSTAINED',
-      icon: null,
-      __typename: 'FilterOption',
-    },
-    {
-      id: 'SUBMITTED',
-      label: 'Submitted',
-      selected: false,
-      value: 'SUBMITTED',
-      icon: null,
-      __typename: 'FilterOption',
+      value: 'BIG_NOUN',
+      label: 'Nouns DAO',
     },
   ],
 };
@@ -71,179 +52,123 @@ const ProposalWrapper = ({
   status?: ProposalState;
   type: string;
   children: any;
-}) => (
-  <div className="flex flex-col border border-[#e2e3e8] rounded-lg cursor-pointer pt-[24px] pb-[24px] px-3">
-    <div className="font-propLot font-bold text-[18px] flex flex-row flex-1 justify-content-start align-items-start">
-      <div className="flex flex-1 flex-col">
-        <div className="flex flex-1">
-          <span className="flex text-[#8C8D92] overflow-hidden">
-            <span className="mr-4">{id}</span>
+}) => {
+  const breakpoint = useBreakpoint();
+  const isMobile = breakpoint === 'S';
+
+  const mobileRow = (
+    <div className="flex flex-col border border-[#e2e3e8] rounded-lg cursor-pointer pt-[24px] pb-[24px] px-3">
+      <div className="font-propLot font-bold text-[18px] flex flex-row flex-1 justify-content-between align-items-start">
+        <span className="flex flex-col sm:flex-row text-[#8C8D92] overflow-hidden gap-[8px]">
+          <span className="flex flex-row gap-[8px] flex-1 justify-content-start align-items-start">
+            <span>{id}</span>
             <span className="truncate">{type === 'LIL_NOUN' ? 'Lil Nouns DAO' : 'Nouns DAO'}</span>
           </span>
-          <span className="text-[#212529] flex flex-1 ml-6">{title}</span>
+          <div className="flex flex-row flex-1 justify-content-start align-items-start">
+            <span className="font-bold text-[18px] text-[#212529] flex flex-1">{title}</span>
+          </div>
+        </span>
+        <div className="flex justify-self-end">
+          <ProposalStatus status={status}></ProposalStatus>
         </div>
       </div>
-      <div className="flex justify-self-end">
-        <ProposalStatus status={status}></ProposalStatus>
-      </div>
+      {children}
     </div>
-    {children}
-  </div>
-);
-
-const ProfileGovernanceList = () => {
-  const { id } = useParams() as { id: string };
-  const snapshotProposalVoteMap = useRef(
-    {} as { [key: string]: { voted: number; reason: string } },
   );
-  const blockNumber = useBlockNumber();
-  const bigNounsTimestamp = useBlockTimestamp(blockNumber);
-  const { timestamp } = useBlockMeta();
-  const [currentTab, setCurrentTab] = useState('YES');
 
-  const [
-    getLilNounGovernanceHistory,
-    { data: lilNounGovernanceHistory, loading: loadingLilNounsHistory },
-  ] = useLazyQuery(LIL_NOUNS_GOVERNANCE_BY_OWNER_SUB, {
-    context: {
-      clientName: 'LilNouns',
-    },
-  });
+  const desktopRow = (
+    <div className="flex flex-col border border-[#e2e3e8] rounded-lg cursor-pointer pt-[24px] pb-[24px] px-3">
+      <div className="font-propLot font-bold text-[18px] flex flex-row flex-1 justify-content-start align-items-start">
+        <div className="flex flex-1 flex-col">
+          <div className="flex flex-1 gap-[8px]">
+            <span className="flex flex-1 text-[#8C8D92] gap-[8px] overflow-hidden">
+              <span>{id}</span>
+              <span className="truncate">
+                {type === 'LIL_NOUN' ? 'Lil Nouns DAO' : 'Nouns DAO'}
+              </span>
+            </span>
+            <span className="text-[#212529] flex flex-1">{title}</span>
+          </div>
+        </div>
+        <div className="flex justify-self-end">
+          <ProposalStatus status={status}></ProposalStatus>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
 
-  const [getBigNounProposalData, { data: bigNounProposalData, loading: loadingBigNounsHistory }] =
-    useLazyQuery(BIG_NOUNS_PROPOSALS_SUB, {
-      context: {
-        clientName: 'NounsDAO',
-      },
-    });
+  return isMobile ? mobileRow : desktopRow;
+};
 
-  const [getNounsSnapshotHistory, { data: nounsSnapshotHistory, loading: loadingSnapshot }] =
-    useLazyQuery(SNAPSHOT_GOVERNANCE_BY_OWNER_SUB, {
-      context: {
-        clientName: 'NounsDAOSnapshot',
-      },
-      onCompleted: data => {
-        const snapshotProposalChoiceMap: { [key: string]: number } = {
-          Against: 0,
-          For: 1,
-          Abstain: 2,
-        };
-        const snapshotProposalData =
-          data?.votes.map((vote: any) => {
-            return { choice: vote.choice, reason: vote.reason, body: vote.proposal.body };
-          }) || [];
-        const proposalUrlRegex = /nouns.wtf\/vote\/[[0-9]+/;
+const filterProposals = (proposals: any[], currentFilter: string) =>
+  proposals
+    .filter(proposal => {
+      if (currentFilter === 'ALL') {
+        return true;
+      }
+      return proposal.type === currentFilter;
+    })
+    .sort((a: any, b: any) => b.createdAt - a.createdAt) || [];
 
-        const snapshotProposalIds = snapshotProposalData
-          .map((proposal: any) => {
-            const matchedUrl = proposal.body.match(proposalUrlRegex);
-            if (Boolean(matchedUrl?.length)) {
-              const proposalId = matchedUrl[0].match(/[0-9]+/);
-              const choice: string = proposal.choice;
-              snapshotProposalVoteMap.current[proposalId] = {
-                voted: snapshotProposalChoiceMap[choice],
-                reason: proposal.reason || '',
-              };
-              return proposalId[0];
-            }
-            return null;
-          })
-          .filter(Boolean);
+const ProfileGovernanceList = ({
+  isLoadingGovernance,
+  snapshotProposalData,
+  categorisedProposals,
+}: {
+  isLoadingGovernance: boolean;
+  snapshotProposalData: any;
+  categorisedProposals: { [key in TabFilterOptionValues]: any[] };
+}) => {
+  const [currentTab, setCurrentTab] = useState(TabFilterOptionValues.YES);
+  const [currentFilter, setFilter] = useState('ALL');
 
-        if (Boolean(snapshotProposalIds.length)) {
-          getBigNounProposalData({
-            variables: {
-              ids: snapshotProposalIds,
-            },
-          });
-        }
-      },
-    });
-
-  useEffect(() => {
-    getLilNounGovernanceHistory({
-      variables: {
-        id: id.toLowerCase(),
-      },
-    });
-
-    getNounsSnapshotHistory({
-      variables: {
-        id: id.toLowerCase(),
-      },
-    });
-  }, [id]);
-
-  const snapshotProposalData = nounsSnapshotHistory?.votes.map((vote: any) => vote.proposal) || [];
-
-  if (loadingLilNounsHistory || loadingSnapshot || loadingBigNounsHistory) {
+  if (isLoadingGovernance) {
     return (
-      <div className={classes.spinner}>
+      <div className="flex flex-1 justify-center mt-[18px]">
         <Spinner animation="border" />
       </div>
     );
   }
 
-  const proposals =
-    lilNounGovernanceHistory?.votes.map((vote: any) => {
-      const proposal = formatSubgraphProposal(vote.proposal, blockNumber, timestamp);
-      return {
-        type: 'LIL_NOUN',
-        proposal,
-        createdAt: proposal.createdBlock,
-        voted: vote.supportDetailed,
-        reason: vote.reason || '',
-      };
-    }) || [];
-
-  const bigNounProposals =
-    bigNounProposalData?.proposals.map((p: any) => {
-      const proposal = formatBigNounSubgraphProposal(p, blockNumber, bigNounsTimestamp);
-      console.log(snapshotProposalVoteMap.current);
-      console.log(proposal.id);
-      return {
-        type: 'BIG_NOUN',
-        proposal,
-        createdAt: proposal.createdBlock,
-        voted: snapshotProposalVoteMap.current[proposal.id]?.voted,
-        reason: snapshotProposalVoteMap.current[proposal.id]?.reason,
-      };
-    }) || [];
-
-  const sortedProposals = [...proposals, ...bigNounProposals]
-    .filter(proposal => {
-      if (currentTab === 'YES') {
-        return proposal.voted === 1;
-      }
-
-      if (currentTab === 'NO') {
-        return proposal.voted === 0;
-      }
-
-      if (currentTab === 'ABSTAIN') {
-        return proposal.voted === 2;
-      }
-
-      if (currentTab === 'SUBMITTED') {
-        return proposal.proposal.proposer === id;
-      }
-
-      return false;
-    })
-    .sort((a, b) => b.createdAt - a.createdAt);
-
-  const handleUpdateFilters = (updatedFilters: string[]) => {
-    setCurrentTab(updatedFilters[0]);
-  };
+  const sortedProposals = filterProposals(categorisedProposals[currentTab] || [], currentFilter);
 
   return (
     <>
       <div className="mt-[32px] mb-[24px] flex flex-col-reverse sm:flex-row">
         <div className="flex mb-[16px] sm:mt-0 mt-[16px] sm:mb-0">
-          <ProfileTabFilters filter={filter} updateFilters={handleUpdateFilters} />
+          <TabWrapper>
+            {TabFilterOptions.map(({ id, label }: { id: TabFilterOptionValues; label: string }) => {
+              return (
+                <TabOption
+                  id={id}
+                  isSelected={currentTab === id}
+                  onClick={e => {
+                    e.preventDefault();
+                    setCurrentTab(id);
+                  }}
+                >{`${label} ${
+                  categorisedProposals[id]?.filter(proposal => {
+                    if (currentFilter === 'ALL') {
+                      return true;
+                    }
+                    return proposal.type === currentFilter;
+                  }).length || 0
+                }`}</TabOption>
+              );
+            })}
+          </TabWrapper>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <DropdownFilter
+            filter={sortFilter}
+            updateFilters={(filters: string[]) => {
+              setFilter(filters[0]);
+            }}
+          />
         </div>
       </div>
-      {sortedProposals.map(p => {
+      {sortedProposals?.map(p => {
         if (p.type === 'LIL_NOUN') {
           return (
             <div className="mb-[16px] space-y-4" key={p.proposal.id}>
